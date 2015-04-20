040dce0d7f9e4abaa53d0f3b5858c1714d2b9307
Moritz Jacobs <mail@moritzjacobs.de>
2015-04-20 14:36:54 +0200

    - Remove compass + add autoprefixer * cleanup

--- gruntfile.js
+++ gruntfile.js
@@ -1,76 +1,90 @@
-	module.exports = function(grunt) {
- 
-    grunt.registerTask('default', ['compass:dev', 'concat', 'watch']);
-    grunt.registerTask('dist', ['compass:dist', 'concat', 'uglify']);
- 
-	grunt.initConfig({
-        pkg:    grunt.file.readJSON('package.json'),
-        config: grunt.file.readJSON('gruntconfig.json'),
+module.exports = function (grunt) {
 
-        compass: {
-            options: {
-                sassDir: 'scss',
-                cssDir: 'css',
-                specify: ['scss/**/*.scss']
-            },
-            dist: {
-				options: {
-					force: true,
-					banner: '<%= config.banner %>',
-					environment: 'production',
-					outputStyle: 'compressed'
-				}
-			},
+	grunt.registerTask('default', ['sass:dev', 'concat', 'watch']);
+	grunt.registerTask('dist', ['sass:dist', 'concat', 'uglify', 'autoprefixer']);
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		config: grunt.file.readJSON('gruntconfig.json'),
+
+		sass: {
 			dev: {
 				options: {
-					environment: 'development',
-					outputStyle: 'expanded'
+					style: 'expanded'
+				},
+				files: [{
+					expand: true,
+					cwd: 'scss',
+					src: ['*.scss'],
+					dest: 'css',
+					ext: '.css'
+				}]
+			},
+			dist: {
+				options: {
+					style: 'compressed',
+                    banner: '<%= config.banner %>',
+				},
+				files: [{
+					expand: true,
+					cwd: 'scss',
+					src: ['*.scss'],
+					dest: 'scss',
+					ext: '.css'
+				}]
+			}
+		},
+		autoprefixer: {
+			dist: {
+				files: {
+					'public/css/styles.css': 'css/styles.css'
 				}
 			}
 		},
 
-        concat: {
-            dist: {
-                options: {
-                    separator: ';'
-                },
-                src: ['js/*.js'],
-                dest: 'js/public/main.js'
-            },
-        },
+		concat: {
+			dist: {
+				options: {
+					separator: ';'
+				},
+				src: ['js/*.js'],
+				dest: 'public/js/main.js'
+			},
+		},
 
 
-        uglify: {
-            options: {
-                mangle: false,
-                banner: '<%= config.banner %>'
-            },
-            js: {
-                files: {
-                    'js/public/main.min.js': ['js/public/main.js']
-                }
-            }
-        },
+		uglify: {
+			options: {
+				mangle: false,
+				banner: '<%= config.banner %>'
+			},
+			js: {
+				files: {
+					'public/js/main.min.js': ['public/js/main.js']
+				}
+			}
+		},
 
 
 		watch: {
 			js: {
-    			files: ['js/*.js'],
+				files: ['js/*.js'],
 				tasks: ['concat'],
-    		},
+			},
 			sass: {
 				files: ['scss/**/*.scss'],
-				tasks: ['compass:dev'],
+				tasks: ['sass:dev', 'autoprefixer'],
 			}
 		},
 
 
 
 	});
- 
+
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-compass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
- 
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
 };
